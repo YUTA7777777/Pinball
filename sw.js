a3277407744f618e579f2e3f1af684d93f93c83d
YUTA <YUTA@example.com>
2018-12-08 11:24:18 +0900

    Added sw.js manifest.json

---
+++ sw.js
@@ -0,0 +1,20 @@
+var CACHE_NAME = 'pinball-caches';
+var urlsToCache = ['/Pinball/',];
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+		.open(CACHE_NAME)
+		.then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+		.match(event.request)
+		.then(function(response) {
+			return response ? response : fetch(event.request);
+		})
+	);
+});
