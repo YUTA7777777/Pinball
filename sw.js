0074896c9e98c07b92e30ea55ef242145a6207ee
YUTA7777777 <43036437+YUTA7777777@users.noreply.github.com>
2019-02-11 12:07:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,41 @@
 var CACHE_NAME = 'pinball-caches';
-var urlsToCache = ['/Pinball/',];
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-		.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
+var appShellFiles = [
+  '/Pinball/',
+  '/Pinball/index.html',
+  '/Pinball/images/app.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
 });
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-		.match(event.request)
-		.then(function(response) {
-			return response ? response : fetch(event.request);
-		})
-	);
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("pinball-caches")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
